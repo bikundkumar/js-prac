8aa3266873c881324ec2b4dee718286c15fd3c60
bikundkumar <bikund2017@outlook.com>
2025-05-26 19:51:15 +0530

    add all

---
+++ index.js
@@ -0,0 +1,45 @@
+let num = 20;
+num = 25;
+console.log(num);
+
+
+let fname = "bikund", lname = "kumar";
+console.log(fname, lname);
+
+const num1 = 20;
+// num1 = 22;
+console.log(num1);
+
+let city = "noida"; // string literal
+console.log(city);
+let nine = 9;   // integer literal
+console.log(nine);
+let age = 19
+let vote = 18 < 19;
+console.log(vote);
+
+const foo = 42;
+const result = foo + "1";
+console.log(result);
+
+// const buttonA = document.querySelector('#button_A');
+// const headingA = document.querySelector("#heading_A");
+
+// let count = 1;
+
+// buttonA.onclick = ()  => {
+//     buttonA.textContent = "try again";
+//     headingA.textContent = `${count} click so far`;
+//     count = +1;
+// };
+
+
+let myStudents = ["bikund", "tushar", "aashany"];
+console.log(myStudents[2]); 
+
+let marks = [20, 33, 34, 44];
+console.log(marks[0]);
+
+for (let i = 0; i <= 3; i++) {
+    console.log(marks[0]);
+}
