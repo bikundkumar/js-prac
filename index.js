510d90a886a2037386903537670e0a6c77ee2f8c
bikundkumar <bikund2017@outlook.com>
2025-05-26 21:40:38 +0530

    new code

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ let num = 20;
 num = 25;
 console.log(num);
 
-
-let fname = "bikund", lname = "kumar";
+let fname = "bikund",
+  lname = "kumar";
 console.log(fname, lname);
 
 const num1 = 20;
@@ -12,11 +12,15 @@ console.log(num1);
 
 let city = "noida"; // string literal
 console.log(city);
-let nine = 9;   // integer literal
+let nine = 9; // integer literal
 console.log(nine);
-let age = 19
+let age = 19;
 let vote = 18 < 19;
 console.log(vote);
+let firstName = undefined;
+console.log(firstName);
+let lastName = null;
+console.log(lastName);
 
 const foo = 42;
 const result = foo + "1";
@@ -33,13 +37,39 @@ console.log(result);
 //     count = +1;
 // };
 
-
 let myStudents = ["bikund", "tushar", "aashany"];
-console.log(myStudents[2]); 
+console.log(myStudents[2]);
 
 let marks = [20, 33, 34, 44];
 console.log(marks[0]);
 
 for (let i = 0; i <= 3; i++) {
-    console.log(marks[0]);
+  console.log(marks[0]);
 }
+
+let Number = "50";
+typeof Number;
+Number = 40;
+typeof Number;
+console.log(Number);
+
+const bird = { species: "Kestrel" };
+console.log(bird.species);
+
+let thala_for_reason = 7;
+// let Jersey_Num = 45;
+
+const cricket = {
+  hitman: "Rohit Sharma$",
+  chikku: 'Virat Kohli$',
+  caption_cool: `Dhoni${thala_for_reason}`,
+};
+
+cricket.hitman = 'Bhada Pav';   // Update 
+console.log(cricket.chikku);
+console.log(cricket.caption_cool);
+console.log(cricket.hitman);
+
+
+
+
